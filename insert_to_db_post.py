b2c717b30e94da30a4d2968b4a51e19014b5d746
gkgkgkgkdlel <gkgkgkgkdlel@naver.com>
2021-12-31 16:54:33 +0900

    db에 csv 파일의 데이터를 넣는 코드

---
+++ insert_to_db_post.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import pymysql.cursors
+
+
+data = pd.read_csv("./_csv_youtube_trending_data.csv")
+
+data = data[['index','video_id','title','publishedAt','categoryId','tags']]
+
+
+connection = pymysql.connect(host='localhost',
+                            user='root',
+                            password='0000',
+                            db='mydb',
+                            charset='utf8mb4',
+                            cursorclass=pymysql.cursors.DictCursor)
+
+try:
+    cursor = connection.cursor()
+    
+    for index, row in data.iterrows():
+        sql = "insert into post(post_index,video_id,title,published_date,category_id,tags) values(%s,%s, %s, %s,%s,%s)"
+        categoryId = int(row['categoryId'])
+        cursor.execute(sql,(row['index'],row['video_id'],row['title'],row['publishedAt'],categoryId,row['tags']))
+        connection.commit()
+
+
+    
+    #print(cursor.fetchall())
+    
+    #rows = cursor.fetchall()
+    #for i in rows :
+     #print(i)
+finally:
+    cursor.close()
+    connection.close()
