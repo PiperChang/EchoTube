8a1a720ab7a77b9e3060f966bc13bd5d659f4e60
gkgkgkgkdlel <gkgkgkgkdlel@naver.com>
2022-01-05 01:19:59 +0900

    완성

--- app.py
+++ app.py
@@ -2,14 +2,24 @@ from flask import Flask, request, jsonify
 from models import Video_Tag, Tag, Video
 from db_connect import db
 from sqlalchemy import and_
-
+import json
 
 
 app = Flask(__name__)
-app.config.from_envvar('APP_CONFIG_FILE')
 app.config['JSON_AS_ASCII'] = False
 
 
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+user = config['database']['user']
+password = config['database']['password']
+host = config['database']['host']
+port = config['database']['port']
+dbname = config['database']['dbname']
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{user}:{password}@{host}:{port}/{dbname}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 result = []
@@ -24,10 +34,13 @@ def home():
 def search_tag():
 
     result.clear()  # 검색 할때마다 result를 초기화
-
+    print('ffff')
     tag = request.args['tag']
     category = request.args['category']
     video = None  # 출력할 1개의 비디오 영상
+
+    print(tag)
+    print(category)
 
     # 입력 받은 tag를 검색
     tag_obj = Tag.query.filter(Tag.name == tag).first()
