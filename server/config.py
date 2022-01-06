9d929bb7c7c6da54d64bf7f6dc76b61fee0cc76e
KCHamp <goddhqj8000@ajou.ac.kr>
2022-01-06 21:53:42 +0900

    uWSGI setting file

--- server/config.py
+++ server/config.py
@@ -14,3 +14,4 @@ db = {
 SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8" 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+git mer
